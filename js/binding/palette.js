7e77665231a77e0d907288a7558fda3a5359b791
Adrian C Shum <me@cshum.com>
2012-11-22 18:15:30 +0800

    tweak palette

--- js/binding/palette.js
+++ js/binding/palette.js
@@ -6,14 +6,17 @@ define(['knockout','jquery','underscore'],function(ko,$,_){
 				.on('touchstart mousedown',function(e){
 					touch = true;
 					call(ko.dataFor(e.target));
+					e.preventDefault();
 				})
 				.on('touchmove mousemove',function(e){
 					if(touch)
 						call(ko.dataFor(e.target));
+					e.preventDefault();
 				});
 			$(window)
 				.on('touchend mouseup',function(){
 					touch = false;
+					e.preventDefault();
 				});
 			ko.utils.domNodeDisposal.addDisposeCallback(el,function(){
 				$(el).off();
