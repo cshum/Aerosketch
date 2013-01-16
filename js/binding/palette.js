10578b0186499e503ca258838d66ddbf242fcc24
Adrian C Shum <adrian@cshum.com>
2013-01-17 00:50:17 +0800

    Test palette

--- js/binding/palette.js
+++ js/binding/palette.js
@@ -1,23 +1,23 @@
 define(['knockout','jquery','underscore'],function(ko,$,_){
 	ko.bindingHandlers.palette = {
 		init: function(el,val) {
-			var call = val(), drag = false;
+			var call = val(), drag = false,
+				y = $(el).offset().top+5;
 			$(el)
 				.on('mousedown',function(e){
 					drag = true;
-					call(ko.dataFor(e.target));
+					var data = ko.dataFor(e.target);
+					if(data) call(data);
 				})
 				.on('mousemove',function(e){
-					if(drag)
-						call(ko.dataFor(e.target));
+					var data = ko.dataFor(e.target);
+					if(drag && data) call(data);
 				})
 				.on('touchstart touchmove',function(e){
-					call(ko.dataFor(
-						document.elementFromPoint(
-							e.originalEvent.touches[0].clientX, 
-							e.originalEvent.touches[0].clientY
-						)
+					var data = ko.dataFor(document.elementFromPoint(
+						e.originalEvent.touches[0].clientX, y
 					));
+					if(data) call(data);
 				});
 			$(window).on('mouseup',function(){
 				drag = false;
