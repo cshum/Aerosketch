8b319754888df132cf66d4a67cc23dc2b9373346
Adrian C Shum <adrian@cshum.com>
2013-01-17 03:03:24 +0800

    revert

--- js/draw.js
+++ js/draw.js
@@ -92,7 +92,6 @@ define([
 		trigger = (function(){
 			var active, timeout;
 			return function(type,e){
-				if(type!='move') console.log(type);
 				function check(t){
 					return ('check' in t) && 
 						t.check(ko.dataFor(e.target));
