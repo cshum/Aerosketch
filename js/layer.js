222cdbc0d646da49729a67c5aafd820a12ab5ab7
Adrian C Shum <adrian@cshum.com>
2013-01-18 17:49:17 +0800

    Tweak layers template

--- js/layer.js
+++ js/layer.js
@@ -8,10 +8,8 @@ function(ko,_,svgTemplate){
 
 		self.shapes = ko.observableArray();
 		self.shapesTemplate = svgTemplate(self.shapes,function(shape){
-			//console.log(shape.visible());
-			//return !shape.visible() ? '':
-			return shape.view || 
-				'<'+shape.getType()+' data-bind="visible:visible,attr:attr" />';
+			return !shape.visible() ? '':
+			shape.view || '<'+shape.getType()+' data-bind="attr:attr" />';
 		});
 	}
 });
