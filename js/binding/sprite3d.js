8f9b651b6b246b15538c927863ffae4383953e3a
Adrian C Shum <adrian@cshum.com>
2013-01-20 21:53:07 +0800

    testing

--- js/binding/sprite3d.js
+++ js/binding/sprite3d.js
@@ -5,6 +5,7 @@ define(['knockout','sprite3d'],function(ko,Sprite3D){
 				sprite = Sprite3D.create(el);
 			ko.computed(function(){
 				var e = ko.toJS(valueAccessor());
+				console.log(e);
 				if(typeof e !== 'object') return;
 				if('origin' in e) sprite.transformOrigin(e.origin.x, e.origin.y);
 				if('translate' in e) sprite.move(e.translate.x, e.translate.y);
