63c6af73bb4b5c9f704a52c0085c38ad3f3fb144
Adrian C Shum <adrian@cshum.com>
2013-01-21 19:00:09 +0800

    test click

--- js/binding/click.js
+++ js/binding/click.js
@@ -5,15 +5,22 @@ define(['knockout','jquery'],function(ko,$){
 				moused, touched;
 			$(el)
 				.on('mousedown',function(e){
+					console.log('mousedown');
 					if(!touched) moused = true;
 				})
 				.on('touchstart',function(e){
+					console.log('touchstart');
 					if(!moused) touched = true;
 				})
-				.on('mouseup touchend',function(e){
-					if(moused || touched) func.call(null,vm,e);
-					moused = false;
+				.on('touchend',function(e){
+					console.log('touchend');
+					if(touched) func.call(null,vm,e);
 					touched = false;
+				})
+				.on('mouseup',function(e){
+					console.log('mouseup');
+					if(moused) func.call(null,vm,e);
+					moused = false;
 				});
 			ko.utils.domNodeDisposal.addDisposeCallback(el, function(){
 				$(el).off();
