2fff3ee35aefa09c9ba6856c848cabe8da72f503
Adrian C Shum <adrian@cshum.com>
2013-02-20 18:47:12 +0800

    test latest hammer

--- js/binding/hammer.js
+++ js/binding/hammer.js
@@ -1,14 +1,15 @@
 define(['knockout','hammer','underscore'],function(ko,Hammer,_){
 	ko.bindingHandlers.hammer = {
 		init: function(el,value,all,vm) {
-			var hammer = new Hammer(el,all().hammerOptions || {});
+			var hammer = Hammer(el,all().hammerOptions || {});
 			_(value()).each(function(func,name){
 				if(_.isFunction(func)){
-					hammer['on'+name] = _(func).bind(null,vm);
+					//hammer['on'+name] = _(func).bind(null,vm);
+					hammer.on(name,_(func).bind(null,vm));
 				}
+				ko.utils.domNodeDisposal.addDisposeCallback(
+					el, _(hammer.off).bind(hammer,name));
 			});
-			ko.utils.domNodeDisposal.addDisposeCallback(
-				el, hammer.destroy);
 		}
 	};
 });
--- js/binding/surface.js
+++ js/binding/surface.js
@@ -9,54 +9,35 @@ function binding(el,value){
 		offset = _.throttle(function(){
 			return $(el).offset();
 		},500),
-		position = function(e){
-			var pos = e.touches || [e];
-			if(!pos[0] || !pos[0].clientX) 
-				return null;
-
-			var sum = _(pos).chain()
-				.map(function(p){
-					return {x:p.clientX, y:p.clientY};
-				})
-				.reduce(function(a,b){
-					return {x:a.x+b.x, y:a.y+b.y};
-				},{x:0,y:0})
-				.value(),
-				len = pos.length;
-
-			return {
-				x: sum.x/len - offset().left,
-				y: sum.y/len - offset().top
-			};
-		},
-		trigger = function(type,isTouch,e){
+		trigger = function(e){
+			var type=e.type;
+			e = e.gesture;
 			//clear transform/drag when drag/transform
-			if((dragging && type.match(/transform/))
-			|| (transforming && type.match(/drag/)))
+			
+			if((dragging && type=='transform')
+			|| (transforming && type=='drag'))
 				trigger('release');
 
 			if(type.match(/drag/)) dragging = true;
 			if(type.match(/transform/)) transforming = true;
+
 			if(type=='release'){
 				dragging = false;
 				transforming = false;
 				drawTrigger('release');
 				return;
 			}
-			target = e.originalEvent.target ||
-				e.originalEvent.touches[0].target;
+			var org =e.srcEvent;
+			//target = org.target || org.touches[0].target;
+			target = e.target;
 
-			pos = position(e.originalEvent) || pos;
-			start = start || pos;
-			if(type=='touch'){
-				if(_.isEqual(start,pos) && isTouch != touchFlag) return; 
-				touchFlag = isTouch;
-				start = pos;
-				inCanvas = $(el).find(target).length>0;
-			}
-			if(type!='wheel' && !inCanvas) return;
+			pos = {
+				x:e.center.pageX - offset().left,
+				y:e.center.pageY - offset().top
+			};
+			if(type=='touch') start = pos;
 
-			var len = (e.originalEvent.touches || [167]).length;
+			var len = e.touches.length;
 			if(len>1 && dragging) return;
 			if(len>2 && transforming) return;
 
@@ -64,9 +45,9 @@ function binding(el,value){
 				dy = pos.y - start.y,
 				evt = {
 					target:target,
-					metaKey:e.originalEvent.metaKey,
-					shiftKey:e.originalEvent.shiftKey,
-					button:e.originalEvent.button,
+					metaKey:org.metaKey,
+					shiftKey:org.shiftKey,
+					button:org.button,
 
 					start: start, position:pos,
 					distanceX:dx, 
@@ -76,38 +57,29 @@ function binding(el,value){
 
 			if(type=='drag')
 				evt.angle = e.angle;
-			if(type=='transform'){
-				if(!(e.scale>0.001 && e.scale<100)) return;
-				_(evt).extend({
-					scale:e.scale,
-					rotation:e.rotation
-				});
-			}
+			_(evt).extend({
+				scale:e.scale,
+				rotation:e.rotation
+			});
+			/*
 			if(type=='wheel')
 				evt.delta = Math.max(-0.2,Math.min(0.2,
-					e.originalEvent.wheelDelta/3500 ||
-					-e.originalEvent.detail/50
+					org.wheelDelta/3500 ||
+					-org.detail/50
 				));
+				*/
 			drawTrigger(type,evt);
 		};
 
 
-	_(new Hammer(document.body,{
+	Hammer(el,{
 		drag_min_distance:10,
 		prevent_default:true,
-		tap_double:false,
-		scale_treshold:0,
-		rotation_treshold:0,
+		doubletap_interval:0,
 		hold:false
-	})).extend({
-		ontap:_(trigger).bind(null,'tap',true),
-		ondragstart:_(trigger).bind(null,'dragstart',true),
-		ondrag:_(trigger).bind(null,'drag',true),
-		ontransformstart:_(trigger).bind(null,'transformstart',true),
-		ontransform:_(trigger).bind(null,'transform',true),
-		onrelease:_(trigger).bind(null,'release',true)
-	});
+	}).on('touch tap dragstart drag transformstart transform release',trigger);
 
+	/*
 	$(document.body)
 		.on('mousedown',_(trigger).bind(null,'touch',false))
 		.on('mousemove',_(trigger).bind(null,'move',false))
@@ -116,6 +88,7 @@ function binding(el,value){
 	$(el)
 		.on('mousewheel DOMMouseScroll',
 			_(trigger).chain().bind(null,'wheel',false).throttle(20).value());
+		*/
 };
 ko.bindingHandlers.surface = {
 	init:binding
