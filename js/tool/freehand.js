d7b6cb510ffcff955c020e5df5a3a117df1c8951
Adrian C Shum <adrian@cshum.com>
2013-03-05 21:18:08 +0800

    freehand simplify

--- js/tool/freehand.js
+++ js/tool/freehand.js
@@ -1,32 +1,36 @@
 define([
-	'underscore','knockout','draw','util/requestanimationframe'
-],function(_,ko,Draw,aniFrame){
-	var point, center, curr, last, prev,
+	'underscore','knockout','draw',
+	'util/requestanimationframe',
+	'util/dist2segment'
+],function(_,ko,Draw,aniFrame,dist2Seg){
+	var point, last, center, i,j,
 		shape, cursor, following,
-		distance = function(p1,p2){
+		dist = function(p1,p2){
 			var dx = p1.x - p2.x,
 				dy = p1.y - p2.y;
 			return Math.sqrt(dx*dx + dy*dy);
-		},
-		perpDistance = function(p0,p1,p){
 		},
 
 		start = function(e){
 			if(Draw.options.stroke()=='none')
 				Draw.options.stroke('black');
 
-			var s = Draw.fromView(e.start);
+			var s = Draw.fromView(e.start),
+				p = Draw.fromView(e.position);
 			shape = Draw.layer().newShape('path');
 			shape.set(Draw.options);
 			shape.fill('none');
 			shape.moveTo(s);
 			shape.lineTo(s);
 
-			cursor = s;
-			point = s;
-			prev = s;
-			center = s;
+			cursor = p;
+			point = p;
 			last = s;
+			center = null;
+
+			i=0;
+			j=0;
+
 			following = true;
 			follow();
 		},
@@ -36,28 +40,29 @@ define([
 		follow = function(){
 			if(!following) return;
 			shape.back();
-			point = {
+			var p = {
 				x: Draw.round(point.x*0.5 + cursor.x*0.5),
 				y: Draw.round(point.y*0.5 + cursor.y*0.5)
 			};
-
-			var d = distance(center, point);
-			if(d >= Draw.options.strokeWidth()){
-				shape.qCurveTo(center,{
-					x: (point.x + center.x)/2,
-					y: (point.y + center.y)/2
-				});
-			   //shape.lineTo(point);
-				last = center;
-				center = point;
+			if(dist(last,point) >= Draw.options.strokeWidth() && 
+			(!center || dist2Seg(last,center,p) >= 0.01/Draw.zoom())){
+				center = {
+					x: (point.x + last.x)/2,
+					y: (point.y + last.y)/2
+				};
+				i++;
+				shape.qCurveTo(last,center);
+				last = point;
 			}
 			shape.lineTo(point);
-			prev = point;
+			point = p;
+			j++;
 
 			if(following)  aniFrame(follow);
 		},
 		release = function(){
 			following = false;
+			console.log(i,j);
 			Draw.save(shape);
 		};
 
