64fc32d7c44bed090197c322d0eaf2899b2720f0
Adrian C Shum <adrian@cshum.com>
2013-03-08 21:13:32 +0800

    Use Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		requirejs:{
+			build: {
+				options:{
+					appDir:'www',
+					dir:'www-build',
+					baseUrl:'js',
+					paths: {
+						'jquery': 'lib/jquery/jquery-1.9.0',
+						'underscore': 'lib/underscore/underscore-amd-min',
+						'underscore.string': 'lib/underscore/underscore.string.min',
+						'knockout':'lib/knockout/knockout-2.2.1',
+						'mousetrap':'lib/mousetrap/mousetrap.min',
+						'hammer':'lib/hammer/hammer.min',
+						'text': 'lib/require/text'
+					},
+					name:'main'
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-requirejs');
+	grunt.registerTask('less','Build less files',function(){
+		require('lessless').optimizeProject('www-build');
+	});
+	grunt.registerTask('build',['requirejs','less']);
+}
