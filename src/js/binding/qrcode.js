b0c6dfc3e20a5e2481129fc53c79275b12fefdfe
Adrian C Shum <adrian@cshum.com>
2013-05-01 16:42:20 +0800

    fix qr code

--- src/js/binding/qrcode.js
+++ src/js/binding/qrcode.js
@@ -3,12 +3,14 @@ define([
 ],function(ko,_,$){
 	ko.bindingHandlers.qrcode = {
 		update: function(el,access) {
-			$(el).qrcode({
-				width: $(el).width(),
-				height: $(el).height(),
-				text: ko.utils.unwrapObservable(access())
-			});
-		}
+			var text = ko.utils.unwrapObservable(access());
+			if(text)
+				$(el).qrcode({
+					width: $(el).width(),
+					height: $(el).height(),
+					text: text
+				});
+			}
 	};
 });
 
